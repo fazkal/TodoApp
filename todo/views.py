50848af750ed0e1edf65cdfc8e392ab8ad764d37
fazkal <fazelkalhory@gmail.com>
2024-12-12 23:11:25 +0330

    Defined TaskCreate class for todo app

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic.edit import (
     CreateView,
     UpdateView,
