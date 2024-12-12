7ced01801ae0adbc243fff54453ac5d3c94a45b2
fazkal <fazelkalhory@gmail.com>
2024-12-12 23:02:16 +0330

    Defined TaskCreate class for todo app

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 # Create your views here.
 
@@ -12,3 +17,12 @@ class TaskList(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
+    
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ["title"]
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
