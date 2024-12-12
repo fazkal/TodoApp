4d83cd12862fc833aa9a8dbfbb62cdf8d2abdf83
fazkal <fazelkalhory@gmail.com>
2024-12-12 19:05:44 +0330

    Added TaskList class to todo's views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "todo/list_task.html"
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
