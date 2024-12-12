f01e3211b7c7ceb697839a1379f62aa93cb62c06
fazkal <fazelkalhory@gmail.com>
2024-12-12 23:05:27 +0330

    Defined RegisterPage for accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 # Create your views here.
+
+class RegisterPage(FormView):
+    template_name = "accounts/register.html"
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("task_list")
+        return super(RegisterPage, self).get(*args, **kwargs)
