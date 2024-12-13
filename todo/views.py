0b9d60287c559ec8b7e28874854287be9ef8c7ff
fazkal <fazelkalhory@gmail.com>
2024-12-13 14:52:53 +0330

    Defined TaskCompelete class for todo's view and edited success_url and template_name for some classes.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -8,6 +8,7 @@ from django.views.generic.edit import (
     UpdateView,
     DeleteView,
 )
+from django.views import View
 from .forms import TaskUpdateForm
 
 # Create your views here.
@@ -15,7 +16,7 @@ from .forms import TaskUpdateForm
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
-    template_name = "todo/list_task.html"
+    template_name = "todo/task_form.html"
 
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
@@ -23,7 +24,7 @@ class TaskList(LoginRequiredMixin, ListView):
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ["title"]
-    success_url = reverse_lazy("task_list")
+    success_url = "/"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -31,14 +32,24 @@ class TaskCreate(LoginRequiredMixin, CreateView):
     
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    success_url = reverse_lazy("task_list")
+    success_url = "/"
     form_class = TaskUpdateForm
     template_name = "todo/update_task.html"
+
+class TaskComplete(LoginRequiredMixin, View):
+    model = Task
+    success_url = reverse_lazy("task_list")
+
+    def get(self, request, *args, **kwargs):
+        object = Task.objects.get(id=kwargs.get("pk"))
+        object.complete = True
+        object.save()
+        return redirect(self.success_url)
 
 class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = "task"
-    success_url = reverse_lazy("task_list")
+    success_url = "/"
 
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
