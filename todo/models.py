6070f9219a43a0e9662839cbf6d79f288c77b90e
fazkal <fazelkalhory@gmail.com>
2025-02-16 01:12:54 +0330

    Changed user field of Task model from django contrib auth to accounts model on model of todo app.

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from accounts.models import User
 
 # Create your models here.
 
 
 class Task(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True, blank=True
+        User, on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
