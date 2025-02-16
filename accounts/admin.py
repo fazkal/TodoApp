eb6f6123cd85f592fb5c6038f7690b5ee11bd3d3
fazkal <fazelkalhory@gmail.com>
2025-02-16 13:26:17 +0330

    Defined CustomUserAdmin class for User model on admin of accounts app.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+'''Customize admin panel for models of accounts app '''
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active")
+    list_filter = ("is_superuser", "is_active")
+    searching_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": ("email", "password"),
+            },
+        ),
+        (
+            "permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                ),
+            },
+        ),
+        (
+            "group permissions",
+            {
+                "fields": ("groups", "user_permissions"),
+            },
+        ),
+        (
+            "important date",
+            {
+                "fields": ("last_login",),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
