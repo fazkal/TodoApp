1c8c0b86ac371671629658f6fab7c4637a5ddc33
fazkal <fazelkalhory@gmail.com>
2025-02-16 15:32:11 +0330

    Registered Profile model on admin of accounts.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User,Profile
 
 # Register your models here.
 
@@ -61,3 +61,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
