4386cc2a67b813b466265075ea77018475b50d0c
fazkal <fazelkalhory@gmail.com>
2025-02-17 15:12:03 +0330

    Defined TaskAdmin class for Task model and register that on admin of todo app.

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+
+    list_display = ['user','title','complete','created_date']
+    searching_fields = ('user','title')
+    list_filter = ('user',)
+
+admin.site.register(Task,TaskAdmin)
