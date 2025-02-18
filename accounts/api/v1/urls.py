8adad2cd19d8441dd0b9b7603523d3bb2c175927
fazkal <fazelkalhory@gmail.com>
2025-02-18 23:56:21 +0330

    Defined url and view for change password on accounts api.

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     # login jwt
     path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
-    path('jwt-verify', TokenVerifyView.as_view(), name='jwt-verify'),
+    path('jwt-verify/', TokenVerifyView.as_view(), name='jwt-verify'),
 
+    #change password
+    path('change-password/',views.ChangePasswordApiView.as_view(), name='change-password'),
 ]
--- accounts/api/v1/views.py
+++ accounts/api/v1/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer,ChangePasswordSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
+User=get_user_model()
+
+# Define class for registration user 
 class RegistrationApiView(generics.GenericAPIView):
     serializer_class=RegistrationSerializer
 
@@ -14,4 +19,25 @@ class RegistrationApiView(generics.GenericAPIView):
                 'email':serializer.validated_data['email']
             }
             return Response(data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+# Define class for change password
+class ChangePasswordApiView(generics.GenericAPIView):
+    model = User
+    permission_classes = [IsAuthenticated]
+    serializer_class = ChangePasswordSerializer
+    
+    def get_object(self,queryset=None):
+        obj = self.request.user
+        return obj
+    
+    def put(self,request,*args,**kwargs):
+        self.object = self.get_object()
+        serializer=self.get_serializer(data= request.data)
+        if serializer.is_valid():
+            if not self.object.check_password(serializer.data.get('old_password')):
+                return Response({"old_password": ["wrong password"]},status=status.HTTP_400_BAD_REQUEST)
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            return Response({'details': 'Password changed successfully'},status=status.HTTP_200_OK)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
