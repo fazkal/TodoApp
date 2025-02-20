7007b96955e1da40840ba186e846fd32502b6ac5
fazkal <fazelkalhory@gmail.com>
2025-02-20 23:55:42 +0330

    Added activation and resend activation urls for accounts api.

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -12,6 +12,12 @@ urlpatterns = [
     # registration
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
 
+    # activation
+    path('activation/confirm/<str:token>', views.ActivationApiView.as_view(),name='activation'),
+
+    # resend activation
+    path('activation/resend/',views.ActivationResendApiView.as_view(),name='activation-resend'),
+
     # login jwt
     path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
