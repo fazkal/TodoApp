0e167a6ed191605a265b9a2013a748e690f16b39
fazkal <fazelkalhory@gmail.com>
2025-02-26 22:27:04 +0330

    Defined absolute_url field on TaskSerializer of todo api.

--- todo/api/serializers.py
+++ todo/api/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from todo.models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.HyperlinkedIdentityField(
+        view_name='todo:api:tasks-detail',lookup_field='pk')
+
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id','title','complete','absolute_url','user']
