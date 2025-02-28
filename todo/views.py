e48f2b29e1bfb9b475bcf65fd6a5b737d5e8ea0d
fazkal <fazelkalhory@gmail.com>
2025-02-28 13:02:11 +0330

    Edited queryset of TaskList and DeleteView and form_valid of TaskCreate on todo's views.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from .models import Task
+from accounts.models import Profile
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import (
@@ -18,16 +19,30 @@ class TaskList(LoginRequiredMixin, ListView):
     context_object_name = "tasks"
     template_name = "todo/task_form.html"
 
-    def get_queryset(self):
-        return self.model.objects.filter(user=self.request.user)
+    #def get_queryset(self):
+        #return self.model.objects.filter(user=self.request.user)
     
+    def get_queryset(self):
+
+         # Get the user's profile
+        try:
+            user_profile = Profile.objects.get(user=self.request.user)
+        except Profile.DoesNotExist:
+            # Handle the case where the user doesn't have a profile
+            # For example, create a new profile or return an empty queryset
+            return Task.objects.none()  # Return an empty queryset
+
+        # Filter tasks by the user's profile
+        return self.model.objects.filter(user=user_profile)
+
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ["title"]
     success_url = "/"
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
+        #form.instance.user = self.request.user
+        form.instance.user = Profile.objects.get(user=self.request.user)
         return super(TaskCreate, self).form_valid(form)
     
 class TaskUpdate(LoginRequiredMixin, UpdateView):
@@ -38,7 +53,7 @@ class TaskUpdate(LoginRequiredMixin, UpdateView):
 
 class TaskComplete(LoginRequiredMixin, View):
     model = Task
-    success_url = reverse_lazy("task_list")
+    success_url = reverse_lazy("todo:task_list")
 
     def get(self, request, *args, **kwargs):
         object = Task.objects.get(id=kwargs.get("pk"))
@@ -55,4 +70,6 @@ class DeleteView(LoginRequiredMixin, DeleteView):
         return self.post(request, *args, **kwargs)
 
     def get_queryset(self):
-        return self.model.objects.filter(user=self.request.user)
+        #return self.model.objects.filter(user=self.request.user)
+        user_profile = Profile.objects.get(user=self.request.user)
+        return self.model.objects.filter(user=user_profile)
