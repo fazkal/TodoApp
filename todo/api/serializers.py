47bb8c7e85a2cc8f2f170dd44238580cd1c1d55b
fazkal <fazelkalhory@gmail.com>
2025-02-28 19:36:36 +0330

    Overrode create function on TaskSerializer of todo api serializers.

--- todo/api/serializers.py
+++ todo/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from todo.models import Task
+from accounts.models import Profile
 
 class TaskSerializer(serializers.ModelSerializer):
     absolute_url = serializers.HyperlinkedIdentityField(
@@ -8,6 +9,7 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id','title','complete','absolute_url','user']
+        read_only_fields = ['user']
 
     def to_representation(self, instance):
         request = self.context.get('request')
@@ -15,3 +17,7 @@ class TaskSerializer(serializers.ModelSerializer):
         if request.parser_context.get('kwargs').get('pk'):
             rep.pop('absolute_url',None)
         return rep
+    
+    def create(self, validated_data):
+        validated_data['user'] = Profile.objects.get(user__id=self.context.get('request').user.id)
+        return super().create(validated_data)
